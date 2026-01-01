9a59b2d62c61339b3f3cf3bfaf1b6c60c69b384e
VARUN <varun.math01@gmail.com>
2026-01-02 01:45:50 +0530

    ChatController.java

--- src/main/java/com/chat/app/controller/ChatController.java
+++ src/main/java/com/chat/app/controller/ChatController.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ChatController {
-
+public class ChatController 
+{
     // /app/sendMessage
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
